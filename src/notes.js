e85b83f6590329b8726a9c892e0ee067470f2ed4
MarianneH1 <marianne.helbig@googlemail.com>
2023-04-14 18:27:54 +0200

    find icons for game

---
+++ src/notes.js
@@ -0,0 +1,6 @@
+// battleship
+import { RiShipFill } from "react-icons/ri";
+// sunkenship
+import { GiSinkingShip } from "react-icons/gi";
+// not hit
+import { CiNoWaitingSign } from "react-icons/ci";
